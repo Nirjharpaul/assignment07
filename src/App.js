c3de42df16c6b99478e15d440f670ed4a5f8b0b6
Nirjhar Paul <nirjhar029@gmail.com>
2021-03-02 11:39:21 -0800

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Players from './components/Players/Players';
+
+function App() {
+  return (
+    <div>
+      <Players></Players>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Players/Players.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import fakeData from '../../fakeData/fakedata.json'
+
+
+const Players = () => {
+    console.log(fakeData);
+
+    return (
+
+        <div>
+            <h1>This is the player card</h1>
+        </div>
+
+    );
+};
+
+export default Players;
